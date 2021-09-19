b1ac9d225b9d8ac6b9904bd496fdf36808b24359
VCyril <ver17hovsk@gmail.com>
2021-09-19 23:05:43 +0300

    Added configs

--- cmd/linksaver/main.go
+++ cmd/linksaver/main.go
@@ -5,16 +5,29 @@ import (
 	"LinkSaver/pkg/handler"
 	"LinkSaver/pkg/repository"
 	"LinkSaver/pkg/service"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"log"
 )
 
 
 func main() {
+	ctx := new(gin.Context)
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error while initializing config: %s", err.Error())
+	}
+
 	repos := new(repository.Repository)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(link.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	defer srv.Shutdown(ctx)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.SetConfigFile("configs/config.yml")
+	return viper.ReadInConfig()
 }

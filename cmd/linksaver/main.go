b1dc0a3e188b693ece2af386b9709e7fdc7fd11b
VCyril <ver17hovsk@gmail.com>
2021-09-20 04:23:23 +0300

    Added postgres, env virables

--- cmd/linksaver/main.go
+++ cmd/linksaver/main.go
@@ -5,23 +5,37 @@ import (
 	"LinkSaver/pkg/handler"
 	"LinkSaver/pkg/repository"
 	"LinkSaver/pkg/service"
-	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
-
 func main() {
-	ctx := new(gin.Context)
 	if err := initConfig(); err != nil {
 		log.Fatalf("Error while initializing config: %s", err.Error())
 	}
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error while loading env virables: %s", err.Error())
+	}
 
-	repos := new(repository.Repository)
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("Error while initializing database: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(link.Server)
-	defer srv.Shutdown(ctx)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,5 +1,6 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
 
 type Authorization interface {
 
@@ -19,6 +20,6 @@ type Repository struct {
 	Link
 }
 
-func NewRepository() *Repository{
+func NewRepository(db *sqlx.DB) *Repository{
 	return &Repository{}
 }
